9009c13908c907ec94144543ad99ac2a135955f5
thucdx <thucdx31@gmail.com>
2014-11-11 15:44:55 +0700

    Update

---
+++ ruby-crawler/src/main/java/fpt/qa/crawler/moveek/MoveekConstants.java
@@ -0,0 +1,21 @@
+package fpt.qa.crawler.moveek;
+
+public class MoveekConstants {
+	public static final String HANOI_URL = "http://moveek.com/ajax/cinemasByRegion/ha-noi";
+	public static final String HCM_URL = "http://moveek.com/ajax/cinemasByRegion/ho-chi-minh";
+	public static final String BIEN_HOA_URL = "http://moveek.com/ajax/cinemasByRegion/bien-hoa";
+	public static final String CAN_THO_URL = "http://moveek.com/ajax/cinemasByRegion/can-tho";
+	public static final String VUNG_TAU_URL = "http://moveek.com/ajax/cinemasByRegion/vung-tau";
+	public static final String BINH_DUONG_URL = "http://moveek.com/ajax/cinemasByRegion/binh-duong";
+	public static final String PHAN_THIET_URL = "http://moveek.com/ajax/cinemasByRegion/phan-thiet";
+	public static final String NHA_TRANG_URL = "http://moveek.com/ajax/cinemasByRegion/nha-trang";
+	public static final String QUY_NHON_URL = "http://moveek.com/ajax/cinemasByRegion/quy-nhon";
+	public static final String DA_NANG_URL = "http://moveek.com/ajax/cinemasByRegion/da-nang";
+	public static final String HAI_PHONG_URL = "http://moveek.com/ajax/cinemasByRegion/hai-phong";
+	public static final String HA_LONG_URL = "http://moveek.com/ajax/cinemasByRegion/ha-long";
+	
+	
+	// cinema urls
+	
+	// end cinema urls section
+}

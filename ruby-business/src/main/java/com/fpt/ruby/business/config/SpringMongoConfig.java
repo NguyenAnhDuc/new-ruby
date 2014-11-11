319569d424d05224de415dc6d3ef2b59c07ac8e7
Administrator <admin@local.host>
2014-11-11 09:34:49 +0700

    update classpath

--- ruby-business/src/main/java/com/fpt/ruby/business/config/SpringMongoConfig.java
+++ ruby-business/src/main/java/com/fpt/ruby/business/config/SpringMongoConfig.java
@@ -18,8 +18,8 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
  @Override
  @Bean
  public Mongo mongo() throws Exception {
-   //return new MongoClient("10.3.9.236");
-   return new MongoClient("localhost");
+   return new MongoClient("10.3.9.236");
+   //return new MongoClient("localhost");
   //return new MongoClient("10.3.9.81");
  }
 }
